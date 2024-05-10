9b32afe9ee96848d15752a7110bb0fb9c2c42414
woolymoth <eliaslinderos@live.se>
2024-05-10 22:35:43 +0200

    Filled in some content and added functions/animations for interests, skills and about

--- portfolio/src/app/pages/about/about.component.ts
+++ portfolio/src/app/pages/about/about.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,} from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AboutComponent {
 
